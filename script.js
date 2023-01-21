bbe5284f74f267ce4e8aa93ddda92b1767538ea4
larrykings <84679942+larrykings@users.noreply.github.com>
2023-01-21 16:35:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -100,6 +100,7 @@ submitBtn.addEventListener('click', () => {
             <h2>You answered ${score}/${quizData.length} questions correctly 
             Show Me Cookie</h2>
             <button onclick="location.reload()">Reload</button>
+            <button onclick="location.href = 'https://module-2-xi.vercel.app/';">Go Back</button>
             `
         }
     }
